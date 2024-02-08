d811eb5f549e0b7e35d7d8c79c4deae700b34748
redowan99 <49404442+redowan99@users.noreply.github.com>
2024-02-08 03:35:56 +0000

    deleting unnecessary

--- Circleftp/src/main/kotlin/com/redowan/CircleFtpProvider.kt
+++ Circleftp/src/main/kotlin/com/redowan/CircleFtpProvider.kt
@@ -12,15 +12,10 @@ class CircleFtpProvider : MainAPI() { // all providers must be an instance of Ma
     override var mainUrl = "http://15.1.1.50:5000/" 
     override var name = "Circle FTP"
     override val supportedTypes = setOf(
-        "TvSeries",
-        "Movie",
-        "AnimeMovie",
-        "Anime",
-        "OVA",
-        "Documentary",
+        TvType.Movie
     )
+    override var lang = "en"
 
-    override var lang = "bn"
 
     // enable this when your provider has a main page
     override val hasMainPage = false
@@ -64,44 +59,3 @@ class CircleFtpProvider : MainAPI() { // all providers must be an instance of Ma
         return extractedData
         //return listOf<SearchResponse>()
     }
-
-    override suspend fun getMainPage(
-        page: Int,
-        request: MainPageRequest
-    ): HomePageResponse {
-        val document = app.get(request.data + page).document
-        val home = document.select("main article").mapNotNull {
-            it.toSearchResult()
-        }
-        return newHomePageResponse(request.name, home)
-    }
-
-
-    override suspend fun load(url: String): LoadResponse? {
-        val document = app.get(url).document
-        val title = document.selectFirst("header > h1")?.text() ?: return null
-        val link = document.selectFirst("article meta[itemprop=embedUrl]")?.attr("content")?.split("/")?.last()?.let{
-            "https://www.youtube.com/watch?v=$it"
-        } ?: throw ErrorLoadingException("No link found")
-
-        return newMovieLoadResponse(title, url, TvType.Movie, link) {
-            this.posterUrl = document.select("div.player-content > img").attr("src")
-            this.year = document.selectFirst("div.meta-bar.meta-single")?.ownText()?.filter { it.isDigit() }?.toIntOrNull()
-            this.plot = document.select("div[itemprop=reviewBody] > p").text().trim()
-            this.tags = document.select("div.meta-bar.meta-single > a").map { it.text() }
-            this.rating = document.selectFirst("div.module div.star")?.text()?.toRatingInt()
-        }
-    }
-
-    override suspend fun loadLinks(
-        data: String,
-        isCasting: Boolean,
-        subtitleCallback: (SubtitleFile) -> Unit,
-        callback: (ExtractorLink) -> Unit
-    ): Boolean {
-
-        loadExtractor(data, data, subtitleCallback, callback)
-
-        return true
-    }
-}
