c926d5627ef7ee9545f24a33dfb0e6955aceacb7
redowan99 <redowan.apu@gmail.com>
2024-08-10 22:57:33 +0600

    fixing build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -66,24 +66,11 @@ subprojects {
     }
 
     dependencies {
-        val apkTasks = listOf("deployWithAdb", "build")
-        val useApk = gradle.startParameter.taskNames.any { taskName ->
-            apkTasks.any { apkTask ->
-                taskName.contains(apkTask, ignoreCase = true)
-            }
-        }
-
-        val implementation by configurations
         val apk by configurations
+        val implementation by configurations
 
-        // If the task is specifically to compile the app then use the stubs, otherwise us the library.
-        if (useApk) {
-            // Stubs for all Cloudstream classes
-            apk("com.lagradost:cloudstream3:pre-release")
-        } else {
-            // For running locally
-            implementation ("com.github.Blatzar:CloudstreamApi:0.1.6")
-        }
+        // Stubs for all Cloudstream classes
+        apk("com.lagradost:cloudstream3:pre-release")
 
         // these dependencies can include any of those which are added by the app,
         // but you dont need to include any of them if you dont need them
