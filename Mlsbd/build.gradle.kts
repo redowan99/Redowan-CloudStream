450e5b0929163401f34ffff1e2b3b6f0b37a0a9b
redowan99 <redowan.apu@gmail.com>
2024-09-09 01:18:40 +0600

    Adding MlsBD

---
+++ Mlsbd/build.gradle.kts
@@ -0,0 +1,33 @@
+// use an integer for version numbers
+version = 1
+
+
+cloudstream {
+    // All of these properties are optional, you can safely remove them
+
+    description = "The largest movie link store in Bangladesh"
+    authors = listOf("Redowan")
+
+    /**
+    * Status int as the following:
+    * 0: Down
+    * 1: Ok
+    * 2: Slow
+    * 3: Beta only
+    * */
+    status = 3 // will be 3 if unspecified
+
+    // List of video source types. Users are able to filter for extensions in a given category.
+    // You can find a list of avaliable types here:
+    // https://recloudstream.github.io/cloudstream/html/app/com.lagradost.cloudstream3/-tv-type/index.html
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries",
+        "NSFW",
+        "AnimeMovie",
+        "AsianDrama"
+    )
+    language = "bn"
+
+    iconUrl = "https://mlsbd.shop/wp-content/uploads/2020/08/MLSBD-Logo.png"
+}
