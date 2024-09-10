4885206d8c16760328ae57e4c0a693e1789e52eb
redowan99 <redowan.apu@gmail.com>
2024-09-10 10:37:39 +0600

    Version 9

--- Oomoye/build.gradle.kts
+++ Oomoye/build.gradle.kts
@@ -1,5 +1,5 @@
 // use an integer for version numbers
-version = 8
+version = 9
 cloudstream {
     // All of these properties are optional, you can safely remove them
     description = "Indian movie site"
--- Oomoye/src/main/kotlin/com/redowan/OomoyeProvider.kt
+++ Oomoye/src/main/kotlin/com/redowan/OomoyeProvider.kt
@@ -1,6 +1,5 @@
 package com.redowan
 
-import android.util.Log
 import com.lagradost.cloudstream3.HomePageResponse
 import com.lagradost.cloudstream3.LoadResponse
 import com.lagradost.cloudstream3.MainAPI
@@ -9,28 +8,24 @@ import com.lagradost.cloudstream3.SearchQuality
 import com.lagradost.cloudstream3.SearchResponse
 import com.lagradost.cloudstream3.SubtitleFile
 import com.lagradost.cloudstream3.TvType
+import com.lagradost.cloudstream3.app
 import com.lagradost.cloudstream3.mainPageOf
 import com.lagradost.cloudstream3.newHomePageResponse
 import com.lagradost.cloudstream3.newMovieLoadResponse
-import com.lagradost.cloudstream3.newTvSeriesSearchResponse
+import com.lagradost.cloudstream3.newMovieSearchResponse
 import com.lagradost.cloudstream3.utils.ExtractorLink
-import com.lagradost.nicehttp.Requests
+import com.lagradost.cloudstream3.utils.Qualities
 import org.jsoup.nodes.Element
 import java.net.URL
 
 
 
 class OomoyeProvider : MainAPI() { // all providers must be an instance of MainAPI
-    override var mainUrl = "https://www.oomoye.info"
+    override var mainUrl = "https://www.oomoye.store"
     override var name = "Oomoye"
-    //override var lang = "bn"
-
-
-    // enable this when your provider has a main page
+    override var lang = "en"
     override val hasMainPage = true
     override val hasDownloadSupport = true
-    override val hasQuickSearch = false
-
 
     override val mainPage = mainPageOf(
         "Bollywood-movies" to "Bollywood Movies",
@@ -52,48 +47,24 @@ class OomoyeProvider : MainAPI() { // all providers must be an instance of MainA
         page: Int,
         request: MainPageRequest
     ): HomePageResponse {
-        val requests = Requests()
-        val doc = requests.get("$mainUrl/category/${request.data}.html?page=$page").document
-        val homeResponse = doc.getElementsByClass("catRow")
-        val home = homeResponse.mapNotNull { post ->
-            toResult(post)
-        }
+        val doc = app.get("$mainUrl/category/${request.data}.html?page=$page").document
+        val home = doc.select(".catRow").mapNotNull {toResult(it)}
         return newHomePageResponse(request.name, home, true)
     }
 
 
     private fun toResult(post: Element): SearchResponse {
         val url = post.select("a").attr("href")
-        val imageIdPattern = "\\d+".toRegex()
-        val imageId = imageIdPattern.find(url)?.value
+        val imageId = "\\d+".toRegex().find(url)?.value
         val title = post.text().replaceAfter(")", "")
-        return newTvSeriesSearchResponse(title, url, TvType.Movie) {
+        return newMovieSearchResponse(title, url, TvType.Movie) {
             this.posterUrl = "$mainUrl/cover/$imageId.png"
-            val check = post.select("font[color=green]").text().lowercase()
-            this.quality = when {
-                "webrip" in check -> SearchQuality.WebRip
-                "web-dl" in check -> SearchQuality.WebRip
-                "bluray" in check -> SearchQuality.BlueRay
-                "hdts" in check -> SearchQuality.HdCam
-                "dvd" in check -> SearchQuality.DVD
-                "cam" in check -> SearchQuality.Cam
-                "camrip" in check -> SearchQuality.CamRip
-                "hdcam" in check -> SearchQuality.HdCam
-                "hdtc" in check -> SearchQuality.HdCam
-                "hdrip" in check -> SearchQuality.HD
-                "hd" in check -> SearchQuality.HD
-                "hdtv" in check -> SearchQuality.HD
-                "rip" in check -> SearchQuality.CamRip
-                "telecine" in check -> SearchQuality.Telecine
-                "telesync" in check -> SearchQuality.Telesync
-                else -> null
-            }
+            this.quality = getQuality(post.select("font[color=green]").text())
         }
     }
 
     override suspend fun load(url: String): LoadResponse {
-        val requests = Requests()
-        val doc = requests.get(url).document
+        val doc = app.get(url).document
         val title = doc.selectXpath("/html/body/div[7]/div").text()
         val year = "(?<=\\()\\d{4}(?=\\))".toRegex().find(title)?.value?.toIntOrNull()
         return newMovieLoadResponse(title, url, TvType.Movie, url) {
@@ -110,15 +81,12 @@ class OomoyeProvider : MainAPI() { // all providers must be an instance of MainA
         subtitleCallback: (SubtitleFile) -> Unit,
         callback: (ExtractorLink) -> Unit
     ): Boolean {
-        val requests = Requests()
-        val doc = requests.get(data).document
+        val doc = app.get(data).document
         doc.select("a[href*=$mainUrl/server/]").forEach { item ->
-            var quality = "\\d{3,4}(?=p)".toRegex().find(item.text())?.value?.toIntOrNull()
-            if (quality == null) quality = 720
-            val links = requests.get(item.attr("href").replace("/server/", "/files/")).document
-            var link = links.select(".fastdl a").attr("href")
+            val links = app.get(item.attr("href").replace("/server/", "/files/")).document
+            val link = links.select(".fastdl a").attr("href")
             val url = URL(link)
-            var hostName = url.host.replace("www.","").substringBefore(".")
+            val hostName = url.host.replace("www.","").substringBefore(".")
             if (link.isNotEmpty())
                 callback.invoke(
                     ExtractorLink(
@@ -126,25 +94,37 @@ class OomoyeProvider : MainAPI() { // all providers must be an instance of MainA
                         hostName,
                         url = link,
                         data,
-                        quality = quality,
+                        quality = getIndexQuality(item.text()),
                         isM3u8 = false,
                         isDash = false
                     )
                 )
-            /*link = links.select("a[href*=$mainUrl/download/]").attr("href")
-            if (link.isNotEmpty())
-                callback.invoke(
-                    ExtractorLink(
-                        this.name,
-                        "CloudFlare",
-                        url = link,
-                        data,
-                        quality = quality,
-                        isM3u8 = false,
-                        isDash = false
-                    )
-                )*/
         }
         return true
+    }
+
+    private fun getIndexQuality(str: String?): Int {
+        return Regex("(\\d{3,4})[pP]").find(str ?: "")?.groupValues?.getOrNull(1)?.toIntOrNull()
+            ?: Qualities.Unknown.value
+    }
+    private fun getQuality(check: String): SearchQuality? {
+        return when(check.lowercase()){
+            in "webrip" -> SearchQuality.WebRip
+            in "web-dl" -> SearchQuality.WebRip
+            in "bluray" -> SearchQuality.BlueRay
+            in "hdts" -> SearchQuality.HdCam
+            in "dvd" -> SearchQuality.DVD
+            in "cam" -> SearchQuality.Cam
+            in "camrip" -> SearchQuality.CamRip
+            in "hdcam" -> SearchQuality.HdCam
+            in "hdtc" -> SearchQuality.HdCam
+            in "hdrip" -> SearchQuality.HD
+            in "hd" -> SearchQuality.HD
+            in "hdtv" -> SearchQuality.HD
+            in "rip" -> SearchQuality.CamRip
+            in "telecine" -> SearchQuality.Telecine
+            in "telesync" -> SearchQuality.Telesync
+            else -> null
+        }
     }
 }
