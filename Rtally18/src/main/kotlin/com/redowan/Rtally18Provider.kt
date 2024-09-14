c62258adeafea9a9841aad3a4a319cac823dfe84
redowan99 <redowan.apu@gmail.com>
2024-09-14 12:49:13 +0600

    Version 4 fixing api again

--- Rtally18/src/main/kotlin/com/redowan/Rtally18Provider.kt
+++ Rtally18/src/main/kotlin/com/redowan/Rtally18Provider.kt
@@ -43,7 +43,8 @@ class Rtally18Provider : MainAPI() {
         page: Int,
         request: MainPageRequest
     ): HomePageResponse {
-        val url = "https://xotpxf3q.api.sanity.io/v2023-03-01/data/query/rtally?query=%0A*%5B_type+%3D%3D+%22movie%22+%26%26+%24keyword+in+categories%5B%5D-%3Etitle%5D+%7C++order%28_createdAt++desc%29+%7C+order%28released++desc%29%5B0...%24end%5D%7B%0A++++...%2C%0A++++type%5B%5D-%3E%2C%0A++++categories%5B%5D-%3E%2C%0A++++genres%5B%5D-%3E%2C%0A++++language%5B%5D-%3E%2C%0A++++quality%5B%5D-%3E%2C%0A++++year%5B%5D-%3E%2C%0A++++%22count%22%3A+count%28*%5B_type+%3D%3D+%22movie%22+%26%26+%24keyword+in+categories%5B%5D-%3Etitle%5D%29%0A%7D&%24keyword=%22${request.data}%22&%24end=96"
+        val pageId = page * 12
+        val url = "https://xotpxf3q.api.sanity.io/v2023-03-01/data/query/rtally?query=%0A*%5B_type+%3D%3D+%22movie%22+%26%26+%24keyword+in+categories%5B%5D-%3Etitle%5D+%7C++order%28_createdAt++desc%29+%7C+order%28released++desc%29%5B${pageId-12}...%24end%5D%7B%0A++++...%2C%0A++++type%5B%5D-%3E%2C%0A++++categories%5B%5D-%3E%2C%0A++++genres%5B%5D-%3E%2C%0A++++language%5B%5D-%3E%2C%0A++++quality%5B%5D-%3E%2C%0A++++year%5B%5D-%3E%2C%0A++++%22count%22%3A+count%28*%5B_type+%3D%3D+%22movie%22+%26%26+%24keyword+in+categories%5B%5D-%3Etitle%5D%29%0A%7D&%24keyword=%22${request.data}%22&%24end=$pageId"
         val doc = app.get(
             url,
             cacheTime = 60,
@@ -53,20 +54,20 @@ class Rtally18Provider : MainAPI() {
             )
         )
         val home = AppUtils.parseJson<Post>(doc.text).result.map { toHomeResult(it) }
-        return newHomePageResponse(request.name, home, false)
+        return newHomePageResponse(request.name, home, true)
     }
 
     private fun toHomeResult(post: Result): SearchResponse {
-        val url = if (post.slug.current.contains("https")) post.slug.current
-            else "$mainUrl/post/${post.slug.current}"
-        val imageId = post.mainImage.asset._ref.replaceFirst("image-","")
-            .replace("-jpg",".jpg")
+        val url = if (post.slug?.current?.contains("https") == true) post.slug.current
+            else "$mainUrl/post/${post.slug?.current}"
+        val imageId = post.mainImage?.asset?._ref?.replaceFirst("image-","")
+            ?.replace("-jpg",".jpg")
         val image = "https://rtally18.vercel.app/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fxotpxf3q%2Frtally%2F${imageId}&w=1080&q=75"
-        return newAnimeSearchResponse(post.title, url, TvType.Movie) {
+        return newAnimeSearchResponse(post.title ?: "", url, TvType.Movie) {
             this.posterUrl = image
             addDubStatus(
                 dubExist = when {
-                    "dual" in post.language.first().title -> true
+                    "dual" in (post.language?.first()?.title ?: "") -> true
                     else -> false
                 },
                 subExist = false
@@ -179,26 +180,26 @@ class Rtally18Provider : MainAPI() {
     )
 
     data class Result(
-        val language: List<Language>,
-        val mainImage: MainImage,
-        val slug: Slug,
-        val title: String
+        val language: List<Language>?,
+        val mainImage: MainImage?,
+        val slug: Slug?,
+        val title: String?
     )
 
     data class Language(
-        val title: String
+        val title: String?
     )
 
     data class MainImage(
-        val asset: Asset
+        val asset: Asset?
     )
 
     data class Slug(
-        val current: String
+        val current: String?
     )
 
     data class Asset(
-        val _ref: String
+        val _ref: String?
     )
 
     private fun selectUntilNonInt(string: String): Int?{
