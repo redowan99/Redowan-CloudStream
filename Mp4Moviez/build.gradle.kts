1266713cd7f32fd7db1ec5616627fda2749a0dec
Salman Ilyas <salman.flutter@magnatecsystems.com>
2024-09-23 16:30:46 +0500

    added mp4moviez provider

---
+++ Mp4Moviez/build.gradle.kts
@@ -0,0 +1,28 @@
+// use an integer for version numbers
+version = 1
+
+cloudstream {
+    description = "Download Mp4Moviez free full movies, high quality movies, latest movies from Mp4Moviez. Mp4moviez is number one entertainment hollywood bollywood website and provide free Mp4moviez full movie download facility"
+    authors = listOf("salman731")
+
+    /**
+    * Status int as the following:
+    * 0: Down
+    * 1: Ok
+    * 2: Slow
+    * 3: Beta only
+    * */
+    status = 1 // will be 3 if unspecified
+
+    // List of video source types. Users are able to filter for extensions in a given category.
+    // You can find a list of available types here:
+    // https://recloudstream.github.io/cloudstream/html/app/com.lagradost.cloudstream3/-tv-type/index.html
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries",
+		"NSFW"
+    )
+    language = "hi"
+
+    iconUrl = "https://www.mp4moviez.glass/images/mp4moviez.png"
+}
---
+++ Mp4Moviez/src/main/kotlin/com/redowan/Mp4MoviezPlugin.kt
@@ -0,0 +1,16 @@
+package com.redowan
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class Mp4MoviezPlugin : Plugin() {
+    override fun load(context: Context) {
+        // All providers should be added in this manner. Please don't edit the providers list directly.
+        registerMainAPI(Mp4MoviezProvider())
+        registerExtractorAPI(StreamTapeSite())
+        registerExtractorAPI(StreamTapeTo())
+        registerExtractorAPI(DoodLi())
+    }
+}
