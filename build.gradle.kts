678104c259c06c7d91a56aca84174143b2c25925
redowan99 <redowan.apu@gmail.com>
2025-03-08 22:54:55 +0600

    Changed build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
-import com.lagradost.cloudstream3.gradle.CloudstreamExtension 
 import com.android.build.gradle.BaseExtension
+import com.lagradost.cloudstream3.gradle.CloudstreamExtension
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 buildscript {
     repositories {
@@ -10,10 +11,10 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.2.2")
+        classpath("com.android.tools.build:gradle:8.7.3")
         // Cloudstream gradle plugin which makes everything work and builds plugins
         classpath("com.github.recloudstream:gradle:-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.10")
     }
 }
 
@@ -43,7 +44,7 @@ subprojects {
     android {
         defaultConfig {
             minSdk = 21
-            compileSdkVersion(33)
+            compileSdkVersion(35)
             targetSdk = 35
         }
 
@@ -52,56 +53,56 @@ subprojects {
             targetCompatibility = JavaVersion.VERSION_1_8
         }
 
-        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        tasks.withType<KotlinCompile>().configureEach {
             kotlinOptions {
                 jvmTarget = "1.8" // Required
                 // Disables some unnecessary features
-                freeCompilerArgs = freeCompilerArgs +
-                        "-Xno-call-assertions" +
-                        "-Xno-param-assertions" +
-                        "-Xno-receiver-assertions"
+                freeCompilerArgs = freeCompilerArgs + listOf(
+                    "-Xno-call-assertions",
+                    "-Xno-param-assertions",
+                    "-Xno-receiver-assertions"
+                )
             }
         }
     }
 
     dependencies {
-        val apk by configurations
+        val runtimeOnly by configurations
         val implementation by configurations
 
-        // Stubs for all Cloudstream classes
-        apk("com.lagradost:cloudstream3:pre-release")
+        val activateCloudstreamApi = true
+        if(activateCloudstreamApi){
+            val apkTasks = listOf("deployWithAdb", "build")
+            val useApk = gradle.startParameter.taskNames.any { taskName ->
+                apkTasks.any { apkTask ->
+                    taskName.contains(apkTask, ignoreCase = true)
+                }
+            }
+            // If the task is specifically to compile the app then use the stubs, otherwise us the library.
+            if (useApk) {
+                // Stubs for all Cloudstream classes
+                runtimeOnly("com.lagradost:cloudstream3:pre-release")
+            } else {
+                // For running locally
+                implementation("com.github.Blatzar:CloudstreamApi:0.1.6")
+            }
+        }
+        else{
+            // Stubs for all Cloudstream classes
+            runtimeOnly("com.github.recloudstream.cloudstream:library:pre-release")
+        }
 
-//        Dev code
-
-//        val apkTasks = listOf("deployWithAdb", "build")
-//        val useApk = gradle.startParameter.taskNames.any { taskName ->
-//            apkTasks.any { apkTask ->
-//                taskName.contains(apkTask, ignoreCase = true)
-//            }
-//        }
-//
-//        val implementation by configurations
-//        val apk by configurations
-//
-//        // If the task is specifically to compile the app then use the stubs, otherwise us the library.
-//        if (useApk) {
-//            // Stubs for all Cloudstream classes
-//            apk("com.lagradost:cloudstream3:pre-release")
-//        } else {
-//            // For running locally
-//            implementation("com.github.Blatzar:CloudstreamApi:0.1.6")
-//        }
 
         // these dependencies can include any of those which are added by the app,
         // but you dont need to include any of them if you dont need them
         // https://github.com/recloudstream/cloudstream/blob/master/app/build.gradle
         implementation(kotlin("stdlib")) // adds standard kotlin features, like listOf, mapOf etc
-
+        implementation("com.github.recloudstream.cloudstream:library:pre-release")
         implementation("com.github.Blatzar:NiceHttp:0.4.11") // http library
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.18.0")
     }
 }
 
 task<Delete>("clean") {
-    delete(rootProject.buildDir)
+    delete(rootProject.layout.buildDirectory)
 }
