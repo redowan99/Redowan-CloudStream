789662baf8ab2c2e0a7813a0d6d9647e942be2dc
redowan99 <redowan.apu@gmail.com>
2025-03-16 22:38:11 +0600

    Version 5: Disabled

--- FilmyHunk/build.gradle.kts
+++ FilmyHunk/build.gradle.kts
@@ -1,5 +1,5 @@
 // use an integer for version numbers
-version = 4
+version = 5
 
 android {
     namespace = "com.redowan"
@@ -16,7 +16,7 @@ cloudstream {
     * 2: Slow
     * 3: Beta only
     * */
-    status = 1 // will be 3 if unspecified
+    status = 0 // will be 3 if unspecified
 
     // List of video source types. Users are able to filter for extensions in a given category.
     // You can find a list of available types here:
