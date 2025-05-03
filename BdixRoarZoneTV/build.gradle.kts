2db85c354424d508595f5e02319017184fb4d75e
redowan99 <redowan.apu@gmail.com>
2025-05-03 10:28:15 +0600

    Adding BdixRoarZoneTV

---
+++ BdixRoarZoneTV/build.gradle.kts
@@ -0,0 +1,27 @@
+// use an integer for version numbers
+version = 1
+
+android {
+    namespace = "com.redowan"
+}
+
+cloudstream {
+    description = "BDIX Live Tv in Bangladesh"
+    authors = listOf("Redowan")
+
+    /**
+    * Status int as the following:
+    * 0: Down
+    * 1: Ok
+    * 2: Slow
+    * 3: Beta only
+    * */
+    status = 1 // will be 3 if unspecified
+
+    tvTypes = listOf(
+        "Live"
+    )
+    language = "bn"
+
+    iconUrl = "https://roarzone.info/logo.webp"
+}
---
+++ BdixRoarZoneTV/src/main/kotlin/com/redowan/BdixRoarZoneTVProvider.kt
@@ -0,0 +1,106 @@
+package com.redowan
+
+import com.lagradost.cloudstream3.HomePageList
+import com.lagradost.cloudstream3.HomePageResponse
+import com.lagradost.cloudstream3.LiveSearchResponse
+import com.lagradost.cloudstream3.LoadResponse
+import com.lagradost.cloudstream3.MainAPI
+import com.lagradost.cloudstream3.MainPageRequest
+import com.lagradost.cloudstream3.SubtitleFile
+import com.lagradost.cloudstream3.TvType
+import com.lagradost.cloudstream3.app
+import com.lagradost.cloudstream3.newHomePageResponse
+import com.lagradost.cloudstream3.newLiveSearchResponse
+import com.lagradost.cloudstream3.newLiveStreamLoadResponse
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.newExtractorLink
+import org.jsoup.nodes.Element
+
+//suspend fun main() {
+//    val providerTester = com.lagradost.cloudstreamtest.ProviderTester(BdixRoarZoneTVProvider())
+////    providerTester.testAll()
+////    providerTester.testMainPage(verbose = true)
+//    providerTester.testLoad("http://tvassets.roarzone.info/images/4.png ; Channel 9 ; bk/4")
+//}
+
+class BdixRoarZoneTVProvider : MainAPI() {
+    override var mainUrl = "http://tv.roarzone.info/"
+    override var name = "(BDIX) RoarZone TV"
+    override var lang = "bn"
+    override val hasMainPage = true
+    override val hasDownloadSupport = false
+    override val hasQuickSearch = false
+    override val supportedTypes = setOf(TvType.Live)
+    private val category = mapOf(
+        "bangla" to "Bangla",
+        "sports" to "Sports",
+        "hindi" to "Hindi",
+        "inbangla" to "Indian Bangla",
+        "english" to "English",
+        "documentary" to "Documentary",
+        "kids" to "Kids",
+        "music" to "Music"
+    )
+
+    override suspend fun getMainPage(
+        page: Int,
+        request: MainPageRequest
+    ): HomePageResponse {
+        val doc = app.get(mainUrl).document
+        val home = mutableListOf<HomePageList>()
+        category.forEach { name ->
+            val response = doc.select("div.item.${name.key}").mapNotNull {
+                toResult(it)
+            }
+            home.add(
+                HomePageList(
+                    name.value,
+                    response,
+                    isHorizontalImages = false
+                )
+            )
+        }
+        return newHomePageResponse(home, hasNext = false)
+    }
+
+    private val hrefRegex = Regex("player\\.php\\?stream=([^']+)")
+    private fun toResult(post: Element): LiveSearchResponse {
+        val imageAttr = post.select("img")
+        val imageLink = imageAttr.attr("src")
+        val name = imageAttr.attr("alt")
+        val link = hrefRegex.find(post.select("a").attr("onclick"))?.groupValues?.get(1) ?: ""
+        val joinedLink = "$imageLink ; $name ; $link"
+        return newLiveSearchResponse(name, joinedLink) {
+            this.posterUrl = imageLink
+        }
+    }
+
+    private val tokenRegex = Regex("token=([^&]+)")
+    override suspend fun load(url: String): LoadResponse {
+        val splitLink = url.split(" ; ")
+        val redirectUrl =
+            app.head("http://tv.roarzone.info/player.php?stream=${splitLink[2]}").url
+        val token = tokenRegex.find(redirectUrl)?.value.toString()
+        val m3uLink =
+            "http://peer19.roarzone.info:8080/roarzone/${splitLink[2]}/index.fmp4.m3u8?$token"
+        return newLiveStreamLoadResponse(name = splitLink[1], url = mainUrl, dataUrl = m3uLink) {
+            this.posterUrl = splitLink[0]
+        }
+    }
+
+    override suspend fun loadLinks(
+        data: String,
+        isCasting: Boolean,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ): Boolean {
+        callback.invoke(
+            newExtractorLink(
+                mainUrl,
+                this.name,
+                url = data
+            )
+        )
+        return true
+    }
+}
