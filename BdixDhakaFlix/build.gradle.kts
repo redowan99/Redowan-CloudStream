6d87ecae798bf538c18f8e8aad8eb21bdc33d283
redowan99 <redowan.apu@gmail.com>
2025-05-06 16:10:07 +0600

    Adding BdixDhakaFlix

--- BdixDhakaFlix/build.gradle.kts
+++ BdixDhakaFlix/build.gradle.kts
@@ -36,5 +36,5 @@ cloudstream {
     )
     language = "bn"
 
-    iconUrl = "http://172.16.50.14/images/2.png"
+    iconUrl = "https://scontent.fdac138-1.fna.fbcdn.net/v/t39.30808-6/444482169_369047609513005_633848578398240822_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=bQMNK-SnESQQ7kNvwGpbhjS&_nc_oc=AdlKZkxCZ-ST1OlSyb-MSHyBVqd1KWn0XuAyeUX4xnGabklW8bqN7dJrqzhBf5a0Ay0&_nc_zt=23&_nc_ht=scontent.fdac138-1.fna&_nc_gid=B8mGdEcxIDp9BcgCel8wFA&oh=00_AfKY4Ftd0DqFnf5xJayXZtq9Eq1NoHOG0XhBkNSzMk3QiQ&oe=68201A97"
 }
