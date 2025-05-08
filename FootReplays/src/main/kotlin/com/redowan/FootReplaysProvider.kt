7591aa42a902c12c649c50efef48e745fa2dbe78
redowan99 <redowan.apu@gmail.com>
2025-05-08 21:54:44 +0600

    Version 4: Fixed

--- FootReplays/src/main/kotlin/com/redowan/FootReplaysProvider.kt
+++ FootReplays/src/main/kotlin/com/redowan/FootReplaysProvider.kt
@@ -22,10 +22,10 @@ import org.jsoup.nodes.Element
 //suspend fun main() {
 //    val providerTester = com.lagradost.cloudstreamtest.ProviderTester(FootReplaysProvider())
 ////    providerTester.testLoadLinks("https://checklinko.top/60382/")
-////    providerTester.testAll()
+//    providerTester.testAll()
 ////    providerTester.testMainPage(verbose = true)
 ////    providerTester.testSearch(query = "rome", verbose = true)
-//    providerTester.testLoad("https://www.footreplays.com/england/fa-cup/manchester-united-vs-fulham-02-03-2025/")
+////    providerTester.testLoad("https://www.footreplays.com/england/premier-league/chelsea-vs-liverpool-04-05-2025/")
 //}
 
 class FootReplaysProvider : MainAPI() { // all providers must be an instance of MainAPI
@@ -55,18 +55,19 @@ class FootReplaysProvider : MainAPI() { // all providers must be an instance of 
         val home = when (request.data) {
             "/" -> {
                 val doc = app.get(mainUrl).document
-                doc.select(".rb-col-t6.rb-col-m12").mapNotNull { toLatestResult(it) }
+                doc.select(".p-wrap.p-overlay.p-overlay-flex a.p-flink")
+                    .mapNotNull { toResult(it) }
             }
 
             "//" -> {
                 val doc = app.get(mainUrl).document
-                doc.select(".elementor-widget-container .widget-post-content .rb-col-m12")
-                    .mapNotNull { toLatestResult(it) }
+                doc.select(".p-wrap.p-small.p-list-small-2 a.p-flink")
+                    .mapNotNull { toResult(it) }
             }
 
             else -> {
                 val doc = app.get("$mainUrl/${request.data}/page/$page/").document
-                doc.select(".content-inner > div").mapNotNull { toResult(it) }
+                doc.select(".p-wrap.p-grid.p-grid-1 a.p-flink").mapNotNull { toResult(it) }
             }
         }
         return newHomePageResponse(
@@ -74,19 +75,10 @@ class FootReplaysProvider : MainAPI() { // all providers must be an instance of 
         )
     }
 
-    private fun toLatestResult(post: Element): SearchResponse {
-        val url = post.selectFirst(".entry-title > a")?.attr("href") ?: ""
-        val title = post.selectFirst(".entry-title > a")?.text() ?: ""
-        val imageUrl = post.selectFirst(".p-flink img")?.attr("fifu-data-src")
-        return newMovieSearchResponse(title, url, TvType.Movie) {
-            this.posterUrl = imageUrl
-        }
-    }
-
     private fun toResult(post: Element): SearchResponse {
-        val url = post.selectFirst(".entry-title > a")?.attr("href") ?: ""
-        val title = post.selectFirst(".entry-title > a")?.text() ?: ""
-        val imageUrl = post.selectFirst(".p-flink img")?.attr("src")
+        val url = post.selectFirst("a")?.attr("href") ?: ""
+        val title = post.selectFirst("a")?.attr("title") ?: ""
+        val imageUrl = post.selectFirst("img")?.attr("src")
         return newMovieSearchResponse(title, url, TvType.Movie) {
             this.posterUrl = imageUrl
         }
@@ -94,29 +86,30 @@ class FootReplaysProvider : MainAPI() { // all providers must be an instance of 
 
     override suspend fun search(query: String): List<SearchResponse> {
         val doc = app.get("$mainUrl/?s=$query").document
-        val searchResponse = doc.select(".content-inner > div")
+        val searchResponse = doc.select(".p-wrap.p-grid.p-grid-small-1 a.p-flink")
         return searchResponse.mapNotNull { toResult(it) }
     }
 
-    private val regex = Regex("""loadVideo\(['"](https?://[^'"]+)['"]\)""")
+    private val regex = Regex("loadVideo\\('([^']+)")
     override suspend fun load(url: String): LoadResponse {
         val doc = app.get(url).document
-        val title = doc.selectFirst("article h1.single-title")?.text() ?: ""
-        val imageUrl = doc.selectFirst(".attachment-pixwell_780x0-2x")?.attr("src")
+        val title = doc.selectFirst("h1.s-title")?.text() ?: ""
+        val imageUrl = doc.selectFirst("div.s-feat >img")?.attr("src")
         val plot = doc.selectFirst(".entry-content > p")?.text()
         val episodesData = mutableListOf<Episode>()
+        var episodeNum = 0
         doc.select("table.video-table tbody > tr").forEach { it ->
-            if (it.text().contains("OK.ru")) {
-                val episodeTitle =
-                    it.select("td:not(:has(a))").mapNotNull { it.text() }.joinToString(" ")
-                val dataHtml = it.selectFirst("td > a")?.attr("onclick")
-                val data = dataHtml?.let { it1 -> regex.find(it1)?.groupValues?.get(1) }
-                episodesData.add(newEpisode(data?.let { it1 ->
-                    newEpisode(it1) {
-                        this.name = episodeTitle
-                    }
-                }))
-            }
+            episodeNum++
+            val episodeTitle = it.select("td:lt(3)").mapNotNull { it.text() }.joinToString(" ")
+            val dataHtml = it.selectFirst("td:nth-child(4) > a:nth-child(1)")?.attr("onclick")
+            val data = dataHtml?.let { it1 -> regex.find(it1)?.groupValues?.get(1) }
+            episodesData.add(newEpisode(data?.let { it1 ->
+                newEpisode(it1) {
+                    this.name = episodeTitle
+                    this.episode = episodeNum
+                }
+            }))
+
         }
         return newTvSeriesLoadResponse(title, url, TvType.TvSeries, episodesData) {
             this.posterUrl = imageUrl
