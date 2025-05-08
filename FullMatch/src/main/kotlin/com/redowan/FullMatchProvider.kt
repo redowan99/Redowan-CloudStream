671cf9b8b37f7ffe51bbb70898a6b389acbb2dee
redowan99 <redowan.apu@gmail.com>
2025-05-08 21:59:06 +0600

    Version 5: Website Shutdown

--- FullMatch/src/main/kotlin/com/redowan/FullMatchProvider.kt
+++ FullMatch/src/main/kotlin/com/redowan/FullMatchProvider.kt
@@ -41,30 +41,6 @@ class FullMatchProvider : MainAPI() { // all providers must be an instance of Ma
         "/italy" to "Italy",
         "/spain" to "Spain",
         "/olympic-2024" to "Olympic 2024"
-
-        //"/mini-match" to "Mini Match",
-        //"/uefa-euro-2024" to "UEFA Euro 2024",
-        //"/uefa-nations-league" to "UEFA Nations League"
-        //"/4k-full-match" to "4K Full Match",
-        //"/africa-cup-of-nations-2025" to "Africa Cup of Nations 2025",
-        //"/belgium-jupiler-league" to "Belgium Jupiler League",
-        //"/dfb-pokal" to "DFB-Pokal",
-        //"/epl-24-25" to "EPL 24/25",
-        //"/eredivisie-24-25" to "Eredivisie 24/25",
-        //"/german-2-bundesliga" to "German 2. Bundesliga",
-        //"/international-friendly" to "International Friendly",
-        //"/la-liga" to "La Liga",
-        //"/la-liga-2" to "La Liga 2",
-        //"/leagues-cup-2024" to "Leagues Cup 2024",
-        //"/portugal" to "Portugal",
-        //"/saudi-pro-league" to "Saudi Pro League",
-        //"/scottish-championship" to "Scottish Championship",
-        //"/scottish-premiership" to "Scottish Premiership",
-        //"/turkish-super-lig" to "Turkish Super Lig",
-        //"/uefa" to "UEFA",
-        //"/uefa-champions-league" to "UEFA Champions League",
-        //"/uefa-conference-league" to "UEFA Conference League",
-        //"/uefa-europa-league-24-25" to "UEFA Europa League 24/25",
     )
 
     override suspend fun getMainPage(
