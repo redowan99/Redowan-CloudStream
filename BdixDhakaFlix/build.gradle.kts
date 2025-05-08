00daaf0373634d9a755b85d462442ce120b41140
redowan99 <redowan.apu@gmail.com>
2025-05-08 21:42:47 +0600

    changing icon link

--- BdixDhakaFlix/build.gradle.kts
+++ BdixDhakaFlix/build.gradle.kts
@@ -36,5 +36,4 @@ cloudstream {
     )
     language = "bn"
 
-    iconUrl = "https://scontent.fdac138-1.fna.fbcdn.net/v/t39.30808-6/444482169_369047609513005_633848578398240822_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=bQMNK-SnESQQ7kNvwGpbhjS&_nc_oc=AdlKZkxCZ-ST1OlSyb-MSHyBVqd1KWn0XuAyeUX4xnGabklW8bqN7dJrqzhBf5a0Ay0&_nc_zt=23&_nc_ht=scontent.fdac138-1.fna&_nc_gid=B8mGdEcxIDp9BcgCel8wFA&oh=00_AfKY4Ftd0DqFnf5xJayXZtq9Eq1NoHOG0XhBkNSzMk3QiQ&oe=68201A97"
-}
+    iconUrl = "https://raw.githubusercontent.com/redowan99/Redowan-CloudStream/refs/heads/master/BdixDhakaFlix/icon.jpg"}
