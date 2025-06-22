585cca333083a5eeab467f7402caaedb9ad6d7ee
redowan99 <redowan.apu@gmail.com>
2025-06-23 00:10:57 +0600

    Added BdixICCFtp

---
+++ BdixICCFtp/build.gradle.kts
@@ -0,0 +1,40 @@
+// use an integer for version numbers
+version = 1
+
+android {
+    namespace = "com.redowan"
+}
+
+cloudstream {
+    // All of these properties are optional, you can safely remove them
+
+    description = "ICC BDIX Provider"
+    authors = listOf("Redowan")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
+
+    // List of video source types. Users are able to filter for extensions in a given category.
+    // You can find a list of avaliable types here:
+    // https://recloudstream.github.io/cloudstream/html/app/com.lagradost.cloudstream3/-tv-type/index.html
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries",
+        "Anime",
+        "AnimeMovie",
+        "OVA",
+        "Cartoon",
+        "AsianDrama",
+        "Others",
+        "Documentary",
+    )
+    language = "bn"
+
+    iconUrl = "http://10.16.100.244/images/logo-purple.svg"
+}
---
+++ BdixICCFtp/src/main/kotlin/com/redowan/BdixICCFtpProvider.kt
@@ -0,0 +1,174 @@
+package com.redowan
+
+
+import com.lagradost.cloudstream3.Episode
+import com.lagradost.cloudstream3.HomePageResponse
+import com.lagradost.cloudstream3.LoadResponse
+import com.lagradost.cloudstream3.MainAPI
+import com.lagradost.cloudstream3.MainPageRequest
+import com.lagradost.cloudstream3.SearchResponse
+import com.lagradost.cloudstream3.SubtitleFile
+import com.lagradost.cloudstream3.TrailerData
+import com.lagradost.cloudstream3.TvType
+import com.lagradost.cloudstream3.app
+import com.lagradost.cloudstream3.getDurationFromString
+import com.lagradost.cloudstream3.mainPageOf
+import com.lagradost.cloudstream3.newEpisode
+import com.lagradost.cloudstream3.newHomePageResponse
+import com.lagradost.cloudstream3.newMovieLoadResponse
+import com.lagradost.cloudstream3.newMovieSearchResponse
+import com.lagradost.cloudstream3.newTvSeriesLoadResponse
+import com.lagradost.cloudstream3.utils.AppUtils
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.newExtractorLink
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.jsoup.nodes.Element
+
+//suspend fun main() {
+//    val providerTester = com.lagradost.cloudstreamtest.ProviderTester(BdixICCFtpProvider())
+////    providerTester.testAll()
+////    providerTester.testMainPage(verbose = true)
+////   providerTester.testSearch(query = "dragon", verbose = true)
+//    providerTester.testLoad("http://10.16.100.244/player.php?play=40575")
+//}
+
+open class BdixICCFtpProvider : MainAPI() {
+    override var mainUrl = "http://10.16.100.244/"
+    override var name = "(BDIX) ICC Ftp"
+    override val hasMainPage = true
+    override val hasDownloadSupport = true
+    override val hasQuickSearch = false
+    override val instantLinkLoading = true
+    override var lang = "bn"
+    override val supportedTypes = setOf(
+        TvType.Movie, TvType.AnimeMovie, TvType.TvSeries, TvType.AsianDrama
+    )
+
+    override val mainPage = mainPageOf(
+        "index.php?category=0" to "Latest",
+//        "English Movies (1080p)/($year) 1080p/" to "English Movies",
+//        "Hindi Movies/($year)/" to "Hindi Movies",
+//        "IMDb Top-250 Movies/" to "IMDb Top-250 Movies",
+//        "SOUTH INDIAN MOVIES/Hindi Dubbed/($year)/" to "Hindi Dubbed",
+//        "SOUTH INDIAN MOVIES/South Movies/$year/" to "South Movies",
+//        "/KOREAN TV %26 WEB Series/" to "Korean TV & WEB Series"
+    )
+
+    override suspend fun getMainPage(
+        page: Int, request: MainPageRequest
+    ): HomePageResponse {
+        val doc = app.get("$mainUrl${request.data}").document
+        val homeResponse = doc.select("div.post-wrapper > a")
+        val home = homeResponse.mapNotNull { post ->
+            getPostResult(post)
+        }
+        return newHomePageResponse(request.name, home, false)
+    }
+
+    private fun getPostResult(post: Element): SearchResponse {
+        val name = post.select("img").attr("alt")
+        val url = mainUrl + post.select("a").attr("href")
+        val image = mainUrl + post.select("img").attr("src")
+        return newMovieSearchResponse(name, url, TvType.Movie) {
+            this.posterUrl = image
+        }
+    }
+
+    private fun getSearchResult(post: SearchJsonItem): SearchResponse {
+        val name = post.name.toString()
+        val url = "${mainUrl}player.php?play=${post.id}"
+        val image = "${mainUrl}files/${post.image}"
+        return newMovieSearchResponse(name, url, TvType.Movie) {
+            this.posterUrl = image
+        }
+    }
+
+    override suspend fun search(query: String): List<SearchResponse> {
+        val mediaType = "application/x-www-form-urlencoded".toMediaType()
+        val body = "cSearch=$query".toRequestBody(mediaType)
+        val doc = app.post("$mainUrl/command.php", requestBody = body).text
+        val json = AppUtils.parseJson<SearchJson>(doc)
+        return json.mapNotNull {getSearchResult(it)}
+    }
+
+    override suspend fun load(url: String): LoadResponse {
+        val doc = app.get(url).document
+        val table = doc.select(".table > tbody:nth-child(1)")
+        val title = table.selectFirst("tr:nth-child(1)")?.text() ?: ""
+        val year = table.selectFirst("tr:nth-child(2) > td:nth-child(2)")?.text()?.toIntOrNull()
+        val genre = table.selectFirst("tr:nth-child(5) > td:nth-child(2)")?.text()?.split(",")
+        val plot = table.selectFirst("tr:nth-child(12) > td:nth-child(2)")?.text()
+        val duration = table.selectFirst("tr:nth-child(4) > td:nth-child(2)")?.text()
+        val trailer = doc.selectFirst(".pull-left")?.attr("data-thevideo")
+        val trailerData = mutableListOf<TrailerData>()
+        if(!trailer.isNullOrEmpty()) {
+            trailerData.add(
+                TrailerData(
+                    extractorUrl = trailer.toString(),
+                    raw = false,
+                    referer = mainUrl
+                )
+            )
+        }
+        val image = mainUrl + doc.selectFirst(".col-md-4 > img")?.attr("src")
+        var downloadEpisode = doc.select(".btn-group > ul > li")
+        if(downloadEpisode.isEmpty()){
+            val link = doc.selectFirst("a.btn")?.attr("href")
+            return newMovieLoadResponse(title, url, TvType.Movie, link) {
+                this.posterUrl = image
+                this.year = year
+                this.plot = plot
+                this.tags = genre
+                this.duration = getDurationFromString(duration)
+                this.trailers = trailerData
+            }
+        }
+        else{
+            downloadEpisode = downloadEpisode.select("span").remove()
+            val episodesData = mutableListOf<Episode>()
+            downloadEpisode.forEach {
+                val link = it.select("a").attr("href")
+                val name = it.select("a").text()
+                episodesData.add(
+                    newEpisode(link){
+                        this.name = name
+                    }
+                )
+            }
+            return newTvSeriesLoadResponse(title, url, TvType.TvSeries, episodesData) {
+                this.posterUrl = image
+                this.year = year
+                this.plot = plot
+                this.tags = genre
+                this.duration = getDurationFromString(duration)
+                this.trailers = trailerData
+            }
+        }
+    }
+
+    override suspend fun loadLinks(
+        data: String,
+        isCasting: Boolean,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ): Boolean {
+        callback.invoke(
+            newExtractorLink(
+                data,
+                this.name,
+                url = data
+            )
+        )
+        return true
+    }
+
+    class SearchJson : ArrayList<SearchJsonItem>()
+
+    data class SearchJsonItem(
+        val id: String?, // 367
+        val image: String?, // 2_guns.jpg
+        val name: String?, // 2 Guns
+        val type: String? // 1
+    )
+}
