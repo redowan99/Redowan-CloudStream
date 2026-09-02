5422bf59682e2eb149c30e59d938968ac9c95f94
redowan99 <redowan.apu@gmail.com>
2026-09-03 02:34:55 +0600

    Remove symbols from main page titles

--- BdixDhakaFlix/src/main/kotlin/com/redowan/BdixDhakaFlix12Provider.kt
+++ BdixDhakaFlix/src/main/kotlin/com/redowan/BdixDhakaFlix12Provider.kt
@@ -10,9 +10,9 @@ class BdixDhakaFlix12Provider : BdixDhakaFlix14Provider()  {
     override val serverName: String = "DHAKA-FLIX-12"
     override val supportedTypes = setOf(TvType.TvSeries)
     override val mainPage= mainPageOf(
-        "TV-WEB-Series/TV Series ★%20 0%20 —%20 9/" to "TV Series ★ 0 — 9",
-        "TV-WEB-Series/TV Series ♥%20 A%20 —%20 L/" to "TV Series ♥ A — L",
-        "TV-WEB-Series/TV Series ♦%20 M%20 —%20 R/" to "TV Series ♦ M — R",
-        "TV-WEB-Series/TV Series ♦%20 S%20 —%20 Z/" to "TV Series ♦ S — Z"
+        "TV-WEB-Series/TV Series ★%20 0%20 —%20 9/" to "TV Series 0 — 9",
+        "TV-WEB-Series/TV Series ♥%20 A%20 —%20 L/" to "TV Series A — L",
+        "TV-WEB-Series/TV Series ♦%20 M%20 —%20 R/" to "TV Series M — R",
+        "TV-WEB-Series/TV Series ♦%20 S%20 —%20 Z/" to "TV Series S — Z"
     )
 }
